edddf629a8fddf12c8de405a391c5646b558a46a
Mykyta Musaiev <n.musaiev@gmail.com>
2025-08-11 10:44:25 +0300

    static mockup with dynamic parsing done

--- src/App.jsx
+++ src/App.jsx
@@ -173,40 +173,22 @@ export const App = () => {
                   <td className="has-text-weight-bold" data-cy="ProductId">
                     {product.id}
                   </td>
-                  <td data-cy="ProductName">Milk</td>
-                  <td data-cy="ProductCategory">🍺 - Drinks</td>
+                  <td data-cy="ProductName">{product.name}</td>
+                  <td data-cy="ProductCategory">
+                    {product.category.icon} - {product.category.title}
+                  </td>
 
-                  <td data-cy="ProductUser" className="has-text-link">
-                    Max
+                  <td
+                    data-cy="ProductUser"
+                    className={cn(
+                      { 'has-text-link': product.user.sex === 'm' },
+                      { 'has-text-danger': product.user.sex === 'f' },
+                    )}
+                  >
+                    {product.user.name}
                   </td>
                 </tr>
               ))}
-
-              <tr data-cy="Product">
-                <td className="has-text-weight-bold" data-cy="ProductId">
-                  2
-                </td>
-
-                <td data-cy="ProductName">Bread</td>
-                <td data-cy="ProductCategory">🍞 - Grocery</td>
-
-                <td data-cy="ProductUser" className="has-text-danger">
-                  Anna
-                </td>
-              </tr>
-
-              <tr data-cy="Product">
-                <td className="has-text-weight-bold" data-cy="ProductId">
-                  3
-                </td>
-
-                <td data-cy="ProductName">iPhone</td>
-                <td data-cy="ProductCategory">💻 - Electronics</td>
-
-                <td data-cy="ProductUser" className="has-text-link">
-                  Roma
-                </td>
-              </tr>
             </tbody>
           </table>
         </div>
